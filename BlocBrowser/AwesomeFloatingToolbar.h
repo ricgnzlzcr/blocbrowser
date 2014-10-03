72246e0072ee65f5f3ce4ee2746b1d9225646ec4
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2014-10-03 12:45:46 -0400

    Pushing floating toolbar branch to GitHub

---
+++ BlocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,13 @@
+//
+//  AwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Ricardo Gonzalez on 10/3/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AwesomeFloatingToolbar : UIView
+
+@end
---
+++ BlocBrowser/AwesomeFloatingToolbar.m
@@ -0,0 +1,21 @@
+//
+//  AwesomeFloatingToolbar.m
+//  BlocBrowser
+//
+//  Created by Ricardo Gonzalez on 10/3/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "AwesomeFloatingToolbar.h"
+
+@implementation AwesomeFloatingToolbar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
